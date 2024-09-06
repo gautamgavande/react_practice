998f939c94e2c7a6f95854339f155b4321c5e0f7
gautamgavande <gautamgavande2436@gmail.com>
2024-09-07 01:33:05 +0530

    this is for practice

---
+++ index.js
@@ -0,0 +1,112 @@
+// let arr=[1,2,3,4,5]
+// // let arr1=arr
+
+// let arr1=[...arr]
+// arr1.pop()
+
+
+// console.log(arr)
+// console.log(arr1)
+
+
+// var arr=[1,2,3,5,4 ]
+
+
+// var state={
+//     name:"gautam",
+//     age:20
+// }
+
+// var copy={
+//     ...state
+// }
+// copy.age=25
+
+// console.log(state)
+// console.log(copy)
+
+// state=copy
+
+// console.log(state)
+// console.log(copy)
+
+
+// var obj={name:"gautam",age:50}
+
+// const {age}=obj
+
+// var obj={
+//     name:"gautam",
+//     age:20,
+//     class:{
+//         first:"grate",
+//         second:"gajab"
+//     }
+// }
+
+// const {second}=obj.class
+// console.log(second)
+// console.log(age)
+
+// var arr=[12,function(){}]
+// var [first,second]=arr
+
+// console.log(first,second)
+
+ //arrow function
+
+//  function abcd(){
+//     console.log("hiiii")
+//  }
+//  abcd()
+
+// const aff=()=>{
+
+// }
+
+// const wwf=()=>{
+
+// }
+
+// const gwgeg=()=>{
+
+// }
+
+// const fwvfdf=()=>{
+//   console.log("hellow")  
+// }
+
+
+// const abcd=(val)=>{
+//   console.log(val)
+// }
+
+// abcd(55)
+
+
+// const abcd=val=>{
+//     console.log(val)
+//   }
+  
+//   abcd(55)
+
+// const abcd=()=>12
+
+// console.log(abcd())
+
+// object return karne ke lia
+
+// const abcd=()=>({name:"gautam"})
+
+// console.log(
+//     abcd()
+// )
+
+// var state=[1,2,3,4]
+// var neww=state.map(e=>e+2)
+// console.log(neww)
+
+
+var arr=[1,2,3,4,5]
+const ans=arr.filter(e=> e>2)
+console.log(ans)
